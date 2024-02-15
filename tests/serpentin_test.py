ffeff1b89f36b645dae7cb5c4ad9f5348027c854
Benoit <benoitleroux41@gmail.com>
2024-02-15 14:03:01 +0100

    adding 3D to serpentin

--- tests/serpentin_test.py
+++ tests/serpentin_test.py
@@ -5,20 +5,25 @@ from time import sleep
 def mesure(*args, **kwargs):
     # il faut laisser assez de délais pour que les mouvements ne soient pas trop brusques
 
-    sleep(.01)
+    sleep(.05)
     return 1
 
-with KPZ101(config_file="tests/x.yaml") as x, KPZ101(config_file="tests/y.yaml") as y:
+with KPZ101(config_file="tests/x.yaml") as x, \
+     KPZ101(config_file="tests/y.yaml") as y, \
+     KPZ101(config_file="tests/z.yaml") as z:
     print(x.conf)
     print(y.conf)
+    print(z.conf)
 
 
     x.enable_output()
     y.enable_output()
+    z.enable_output()
 
-    s = Scan((x, y), config_file="tests/scan.yaml")
+    s = Scan((x, y, z), config_file="tests/scan.yaml")
 
     # Launching scan on nanomax
+    # s.visualize()
     m = s.scan(mesure)
 
     # m contains the matrix of measurement
